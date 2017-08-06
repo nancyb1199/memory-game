e2a214c6b8f95b39b9879513830cfe814fe87db2
Nancy <belscher@nancys-mbp.attlocal.net>
2017-08-05 23:07:51 -0400

    able to flip cards once but not back yet

--- script.js
+++ script.js
@@ -34,25 +34,59 @@ function shuffle (array) {
     array[j] = temp
   }
 }
+/* end Fisher-Yates shuffle */
 
-shuffle (cardbacks);
-console.log(cardbacks);
+/* Setting up the board */
+shuffle (matches);
+// console.log(matches);
 for (i=0; i<cardbacks.length; i++){
   var y = "card"+cardbacks[i];
-  console.log(y);
-  let x = document.getElementById( y );
-  console.log(x);
-  x.innerHTML = matches[i];
-/*  x.setAttribute ("class", "hidden"); */
+  let board = document.getElementById("board");
+  // console.log(y);
+  let x = `
+  <div id = ${y} class="hidden">
+  <h2>${matches[i]}</h2>
+  </div>
+  `
+  board.innerHTML += x;
+
 }
 
 
-
-let block1 = document.getElementById( "carda" );
+let activeCount = 0;
+let block1 = document.getElementById( "board" );
 block1.addEventListener("click", myCallBack);
+let firstFlip;
+let secondFlip;
+let twoFlipped = false;
 
 function myCallBack() {
-       block1.style.backgroundColor = "pink";
 
+  if (firstFlip == null) {
 
+      firstFlip = event.target.id;
+      console.log(firstFlip);
+      let thisCard = document.getElementById(firstFlip);
+      console.log(thisCard);
+      let firstFlipID = thisCard.getElementsByTagName('h2')[0];
+      console.log(firstFlipID);
+    }
+    else {
+      secondFlip = event.target.id;
+      let thisCard = document.getElementById(secondFlip);
+      let secondFlipID = thisCard.getElementsByTagName('h2')[0];
+      console.log(secondFlipID);
+      let twoFlipped = true;
+    }
+    event.target.className = "active";
 }
+  if (twoFlipped){
+    if (firstFlipID == secondFlipID){
+      activeCount++;
+    }
+    else {
+      card1 = document.getElementById(firstFlip);
+      card2 = document.getElementById(secondFlip);
+      
+    }
+  }
